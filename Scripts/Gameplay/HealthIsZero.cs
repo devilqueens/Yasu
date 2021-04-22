cad5bae25af44ef4d31e249f5377da7fe9be6bbd
HuarteB <56865522+HuarteB@users.noreply.github.com>
2021-04-22 21:39:02 +0200

    Add files via upload

---
+++ Scripts/Gameplay/HealthIsZero.cs
@@ -0,0 +1,28 @@
+using Platformer.Core;
+using Platformer.Mechanics;
+using static Platformer.Core.Simulation;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Platformer.Gameplay
+{
+    /// <summary>
+    /// Fired when the player health reaches 0. This usually would result in a 
+    /// PlayerDeath event.
+    /// </summary>
+    /// <typeparam name="HealthIsZero"></typeparam>
+    public class HealthIsZero : Simulation.Event<HealthIsZero>
+    {
+        public Health health;
+
+        public override void Execute()
+        {
+            SceneManager.LoadScene(3);
+
+        }
+
+
+    }
+}
